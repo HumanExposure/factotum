69b7a8e43fce41ff88220988c60b7a48830653f5
michael-barbour <michaelbarbour163@gmail.com>
2020-02-04 20:16:36 -0500

    1336 stats table refinements (#1370)
    * Made table changes and added caching
    
    * Remove loading table header and black reformat
    
    * Added tests and removed front page caching
    
    * reworked datatables ajax for grouptype_table
    
    * Rollback caching changes
    
    * Correct comment
    
    * removed console log

--- static/js/dashboard/index.js
+++ static/js/dashboard/index.js
@@ -95,11 +95,35 @@ function renderVenn(div_id, set_data) {
     //         return d.size
     //     });
 }
+
+
+function grouptype_transform(rows)
+{
+    for (row of rows) {
+        if (['Habits and practices', 'Unidentified', 'Supplemental documents'].includes(row[0]))
+        {
+            row[2] = '';
+            row[3] = '';
+        }
+    }
+    return rows
+}
+
 $('#grouptype_table').DataTable({
-    "serverSide": false,
-    "info": false,
-    "paging": false,
-    "searching": false,
-    "ordering": false,
-    "ajax": "grouptype/stats/",
-});
+    "serverSide":   false,
+    "info":         false,
+    "paging":       false,
+    "searching":    false,
+    "ordering":     false,
+    "ajax":         {
+                        "url":          "grouptype/stats/",
+                        "dataSrc":      function ( json ) {
+                            return this.grouptype_transform(json.data);
+                        },
+                    },
+    "columns":      [{"title": "Group Type"},
+                     {"title": "Documents (%)"},
+                     {"title": "Raw Chemical Records (%)"},
+                     {"title": "Curated Chemical Records (%)"},]
+})
+
